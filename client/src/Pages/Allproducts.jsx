0577d7f401b1eec256ad2ba076d2701bc011d492
Mohini <mohinidhiman115@gmail.com>
2025-09-27 01:53:58 +0530

    Fix frontend

--- client/src/Pages/Allproducts.jsx
+++ client/src/Pages/Allproducts.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import "./AllProducts.css";
+import "./Allproducts.css";
 
 const AllProducts = () => {
   const [products, setProducts] = useState([]);
