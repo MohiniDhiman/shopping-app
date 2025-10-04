9a533d1ddba0dbc5e20ac5502373883d8d1ba6c4
Mohini <mohinidhiman115@gmail.com>
2025-10-04 17:36:43 +0530

    verification error

--- client/src/Components/BuyPage/PaymentOptions.jsx
+++ client/src/Components/BuyPage/PaymentOptions.jsx
@@ -53,7 +53,7 @@ const PaymentOptions = ({ amount, onPaymentSuccess, items }) => {
         order_id: data.id,
         handler: async function (response) {
           try {
-            const verifyRes = await fetch(`${apiUrl}/payment/verify-payment`, {
+            const verifyRes = await fetch(`${apiUrl}/payment/verify`, {
               method: "POST",
               headers: { "Content-Type": "application/json" },
               body: JSON.stringify({
