5751f176b05b3d0d9f0f8442a957941caa4806a3
Mohini <mohinidhiman115@gmail.com>
2025-10-04 19:46:30 +0530

    fixes

--- backend/server.js
+++ backend/server.js
@@ -31,9 +31,7 @@ app.use("/api/offers", offersRoute);
 
 app.use("/api/address", require("./routes/AddressRoute"));
 app.use("/api/orders", require("./routes/OrderRoute"));
-app.use("/api/payment", require("./routes/PaymentRoute"));
 app.use("/api/cart", require("./routes/CartRoute"));
-// app.use("/api/payment", require("./routes/PaymentRoute"));
 
 // PostgreSQL Connection
 const pool = new Pool({
