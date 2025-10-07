1f6b249000f3a3ae486ad656eb81b7a709432c23
Mohini <mohinidhiman115@gmail.com>
2025-10-07 17:29:15 +0530

    backend fix

--- backend/routes/ProductRoutes.js
+++ backend/routes/ProductRoutes.js
@@ -12,7 +12,8 @@ if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir, { recursive: true });
 // ========== IMAGE UPLOAD SETUP ==========
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, uploadDir),
-  filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname),
+  filename: (req, file, cb) =>
+    cb(null, Date.now() + "-" + file.originalname.replace(/\s/g, "_")),
 });
 const upload = multer({ storage });
 
